c9aa4f7cc5cbcc44430e19ec516018c29dbaddb6
Fernando Rodriguez <32549106+FerRodriguez96@users.noreply.github.com>
2024-12-05 17:54:19 -0300

    first commit

---
+++ src/main/java/com/pylp/turnos_firebase/TurnosFirebaseApplication.java
@@ -0,0 +1,13 @@
+package com.pylp.turnos_firebase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnosFirebaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnosFirebaseApplication.class, args);
+	}
+
+}
