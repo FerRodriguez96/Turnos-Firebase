22f4e8d639c6e8382eac5e04f53b628b7f865f8d
Fernando Rodriguez <32549106+FerRodriguez96@users.noreply.github.com>
2024-12-05 21:22:04 -0300

    dependencias

---
+++ src/main/java/com/pylp/service/FirebaseService.java
@@ -0,0 +1,23 @@
+package com.pylp.service;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseService {
+
+    private final DatabaseReference database;
+
+    public FirebaseService() {
+        this.database = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void saveData(String path, Object data) {
+        database.child(path).setValueAsync(data);
+    }
+
+    public DatabaseReference getData(String path) {
+        return database.child(path);
+    }
+}
